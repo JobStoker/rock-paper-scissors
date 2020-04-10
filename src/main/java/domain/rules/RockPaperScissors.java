955124ccb7ee03a045744e152e49dd226d3f4b17
JobStoker <job.stoker@gmail.com>
2020-04-10 14:46:08 +0200

    first commit

---
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -0,0 +1,19 @@
+package domain.rules;
+
+import domain.Move;
+import domain.Result;
+import domain.Rules;
+
+public class RockPaperScissors implements Rules {
+    @Override
+    public Result decide(Move mine, Move theirs) {
+        if (mine.equals(theirs)) return Result.DRAW;
+
+        switch (mine) {
+            case SCISSORS: return theirs.equals(Move.PAPER) ? Result.WIN : Result.LOSE;
+            case PAPER: return theirs.equals(Move.ROCK) ? Result.WIN : Result.LOSE;
+            case ROCK: return theirs.equals(Move.SCISSORS) ? Result.WIN : Result.LOSE;
+            default: return Result.DRAW;
+        }
+    }
+}
